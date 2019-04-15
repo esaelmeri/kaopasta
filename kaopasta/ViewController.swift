38df4977bab21a3de1f1c3c442879c895b797571
Esa Aikio <esa.aikio@gmail.com>
2019-04-15 22:06:31 +0300

    15.4. Empty project

---
+++ kaopasta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kaopasta
+//
+//  Created by Esa Aikio on 15/04/2019.
+//  Copyright © 2019 Esa Aikio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
